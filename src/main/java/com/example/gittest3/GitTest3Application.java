6b5f7772ca586e3d0d3500160b981f06e919ed81
kylin4chen <85142101@qq.com>
2023-02-27 12:57:57 +0800

    第一次提交

---
+++ src/main/java/com/example/gittest3/GitTest3Application.java
@@ -0,0 +1,13 @@
+package com.example.gittest3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitTest3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitTest3Application.class, args);
+	}
+
+}
